84e42c78cc367b1765fb6320bd104df2a2de8078
paulinaheine <paulina.heine@uni-bielefeld.de>
2023-05-12 11:39:05 +0200

    update

--- Text rec/Regex_name_transformation.py
+++ Text rec/Regex_name_transformation.py
@@ -5,12 +5,38 @@ Funktion die aus Namen Rexgex slices macht. Z.B.: Sun -> (?i).*s.*u.*n.*
 import regex as re
 
 digits = {0: ["O","o"], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
+characters = {"a": ["a","o","r"], #A
+              "b": ["b","L","R"], #B
+              "c": ["c","L"],
+              "d": ["d","l","B","R"],
+              "e": ["e","a","r"],
+              "f": ["f","R","r","I"],
+              "g": ["g"],
+              "h": ["h"],
+              "i": ["i"],
+              "j": ["j"],
+              "k": ["k"],
+              "l": ["l"],
+              "m": ["m"],
+              "n": ["n"],
+              "o": ["o"],
+              "p": ["p"],
+              "q": ["q"],
+              "r": ["r","F"],
+              "s": ["s"],
+              "t": ["t"],
+              "u": ["u"],
+              "v": ["v"],
+              "w": ["w"],
+              "x": ["x"],
+              "y": ["y"],
+              "z": ["z"],}
 
 def regname_1(name):
     '''
     Function that returns the devicename in Regex form
     :param name: Input device name
-    :return: device name in regex form
+    :return: device name in regex form as a rexex type
     '''
     rex = ["(?i).*"]
     splitted = re.findall(".", name)
@@ -19,3 +45,25 @@ def regname_1(name):
         rex.append(".*")
     rex = "".join(rex)
     return rex
+
+def regname_2(name):
+    '''
+    Function that returns the devicename in Regex form with multiple possibilities for missclassified caracters
+    :param name: Input device name
+    :return: device name in regex form as a rexex type
+    '''
+    rex = ["(?i).*"]
+    splitted = re.findall(".", name)
+    for c in range(len(splitted)):
+        for i in characters.keys():
+            if str(i).upper() == splitted[c].upper():
+                rex.extend(f"{characters[i]}")
+        rex.append(".*")
+
+    #for x in range(len(rex)):
+    #    rex[x] = "".join(rex[x])
+    rex = "".join(rex)
+    rex = re.compile(rex)
+    return rex
+
+
--- Text rec/search_data.py
+++ Text rec/search_data.py
@@ -1,4 +1,4 @@
-from Regex_name_transformation import regname_1
+from Regex_name_transformation import regname_1, regname_2
 import regex as re
 from difflib import SequenceMatcher
 
@@ -9,19 +9,50 @@ data = {
     "CiscoASR9000Series": "bliblablub"}
 
 def search(name, data):
+    '''Funktion gibt Matches aus wenn rex alle characters enthalten
+    oder mehr'''
     for key in data.keys():
         reObj = re.compile(regname_1(key))
-        if reObj.match(name):
+        print(reObj)
+        if reObj.search(name):
             print(key, data[key])
 
 
 search("CiscoASR9000series", data)
+
+def search(name, data):
+    '''Funktion gibt Matches aus wenn rex alle characters enthalten
+    oder mehr'''
+    for key in data.keys():
+        reObj = re.compile(regname_2(key))
+        print(reObj)
+        if reObj.search(name):
+            print(key, data[key])
+
+
+search("CiscoASR9000series", data)
+
+
 '''
 def similar(a,b):
     a = regname_1(a)
     print(a)
     b = regname_1(b)
+    print(b)
     return SequenceMatcher(None,a,b).ratio()
+
+
+def sim(text,key):
+    a = re.compile(regname_1(text))
+    b = re.compile(regname_1(key))
+    re.search(pattern, string, flags=0)
+
+def search(name, data):
+    name = re.compile(regname_1(name))
+    for key in data.keys():
+        #reObj = re.compile(regname_1(key))
+        if re.split(name, key):
+            print(key, data[key])
+
+
 '''
-
-
