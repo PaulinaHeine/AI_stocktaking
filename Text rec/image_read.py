4a602d1f83246c7daca35e31c7c7fee156055242
Paulina Heine <105652581+PaulinaHeine@users.noreply.github.com>
2023-05-23 10:47:19 +0200

    Update image_read.py

--- Text rec/image_read.py
+++ Text rec/image_read.py
@@ -10,13 +10,14 @@ img = cv2.imread('/Users/paulinaheine/s-l1600.jpg')
 #img = cv2.imread('/Users/paulinaheine/IMG_3120.png')
 #img_canny = canny(img)
 #img = cv2.imread("/Users/paulinaheine/Bildschirm­foto 2023-05-10 um 12.26.59.png")
+
 img_grey = get_grayscale(img)
 img_canny = canny(img_grey)
-#plt.imshow(img)
+# plt.imshow(img)
 
-images = [img_canny,img_grey]
-configs = [r'--oem 3 --psm 6',r'--psm 4', r"--psm 11 --oem 3"
-]
+images = [img_grey,img_canny]
+configs = [r'--oem 3 --psm 6', r'--psm 4', r"--psm 11 --oem 3"]
+
 
 def text_rec(images, configs):
     '''
@@ -26,29 +27,32 @@ def text_rec(images, configs):
     res_set = set()
 
     for con in configs:
+        print("loading")
         for i in images:
-
+            print("...loading...")
             # Alle sinnvollen Einstellungen werden durchprobiert
-            print("read")
+            #print("read")
             res_list = pytesseract.image_to_string(i, config=con)
             # Alle Einträge werden in einzelne snippets unterteilt
-            print("split")
+            #print("split")
             res_list_2 = res_list.split("\n")
             # Alle leeren Einträge werden entfernt
-            print("blanks")
+            #print("blanks")
             while '' in res_list_2:
                 res_list_2.remove('')
-            print("isalnum")
+            #print("isalnum")
+            #Sonderzeichen entfernen
             for x in range(len(res_list_2)):
                 res_set.add(''.join(filter(str.isalnum, res_list_2[x])))
 
     # Alle Einträge mit weniger als 3 Einträgen werden entferht
-    print("charlimit")
+    #print("charlimit")
     res_set = list(filter(lambda el: len(el) > 3, res_set))
     res_set = list(filter(lambda el: len(el) < 30, res_set))
     print("done")
     res_set = sorted(res_set)
-
+    print(f"We have {len(res_set)}entries")
     return res_set
 
 
+#x = text_rec(images, configs)
