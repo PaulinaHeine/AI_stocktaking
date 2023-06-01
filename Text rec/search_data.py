70395a11443c8ba17c5625d8e1d1578b12a01d08
Paulina Heine <105652581+PaulinaHeine@users.noreply.github.com>
2023-06-01 10:50:12 +0200

    Update search_data.py

--- Text rec/search_data.py
+++ Text rec/search_data.py
@@ -1,39 +1,49 @@
+
+'''Suchalgorithmen & Vergleichsalgorithmen. Alles was ausgelesen wurde, muss mit der Datenbank verglichen werden,
+dafür gibt es mehrere Algorihtmen:
+-search(name, data) = Es müssen genau die Buchstaben in genau der Rheinfolge erhalten werden, dann MAtch keine fehlenden Buchstaben erlaubt
+-search_more_loop(names, data)= Es müssen die Buchstaben oder ähnliche (Z.B.: o wie e) in genau der Rheinfolge erhalten werden, dann MAtch keine fehlenden Buchstaben erlaubt
+-proof_avb(data, p,  res_set ) = Passen die Buchstaben, Rheinfolge egal, überein?  Fehlenden Buchstaben erlaubt je nachdem wieviele fehlen
+ '''
+
 from Regex_name_transformation import regname_1, regname_2
 import regex as re
 from difflib import SequenceMatcher
-
+from Datenbank import df
 from Text.image_read import text_rec, images, configs
 
 data = {
     "SunfireV60x": "lalalalaal",
     "SunfireV65x": "blablablabla",
-    "SparcEnterpriseT200": "lililililili",
+    "SparcEnterpriseT2000": "lililililili",
     "CiscoASR9000Series": "bliblablub",
-    "SunFireX4100":"nananananan"}
-
+    "SunFireX4100": "nananananan",
+    "Cisco":"Fisco",
+    "SunMicrosystems": "Fun"
+}
 
 
 def search(name, data):
     '''Funktion gibt Matches aus wenn rex alle characters enthalten
     oder mehr'''
-    #reobj = []
+    # reobj = []
     for key in data.keys():
         reObj = re.compile(regname_1(key))
-        #print(reObj)
+        # print(reObj)
         if reObj.search(name):
             print("Match found in run1")
             print(key, data[key])
             break
         else:
             for key in data.keys():
-                #reObj = re.compile(regname_2(key))
-                #print(reObj)
+                # reObj = re.compile(regname_2(key))
+                # print(reObj)
                 if reObj.search(name):
                     print("Match found in run2")
                     print(key, data[key])
 
 
-#search("CiscoASR9000series", data)
+# search("CiscoASR9000series", data)
 
 
 def search_more(name, data):
@@ -41,7 +51,7 @@ def search_more(name, data):
     oder mehr und missclassified okay'''
     for key in data.keys():
         reObj = re.compile(regname_1(key))
-        #print(reObj)
+        # print(reObj)
         if reObj.search(name):
             print("Match found in run1")
             return key, data[key]
@@ -49,29 +59,29 @@ def search_more(name, data):
     for key in data.keys():
         print("No match found in run1, trying run2 (missclassificatins okay)")
         reObj = re.compile(regname_2(key))
-        #print(reObj)
+        # print(reObj)
         if reObj.search(name):
             return key, data[key]
             break
 
 
-search_more("CiseoASR9000Series", data)
+#search_more("CiseoASR9000Series", data)
 
 
-def search_more_loop(names,data):
+def search_more_loop(names, data):
     '''
-    Funktion geht alle geesenen strings durch und sucht nach einem match in der datei
+    Funktion geht alle geesenen strings durch und sucht nach einem match in der datei auch mis misclassifieds
     :param names: list of returned strings from mage read
     :param data: datenbank
     :return: match
     '''
     y = []
     for i in range(len(names)):
-        name = names[i]
+        name = "".join(names[i])
         for key in data.keys():
             reObj = re.compile(regname_1(key))
             if reObj.search(name):
-                y.append([names[i],key, data[key]])
+                y.append([names[i], key, data[key]])
                 print(f"MATCH FOUND FOR {name}")
                 break
             pass
@@ -87,8 +97,56 @@ def search_more_loop(names,data):
 
     return y
 
-y=search_more_loop(x,data)
+
+#y = search_more_loop(x, data)
+#y = search_more_loop(names=text_rec(images, configs), data=data)
 
 
-y=search_more_loop(names=text_rec(images, configs),data=data)
+def similarity(name):
+    '''
+    Gibt eine Liste aller chararcter in einer sequenz einzeln aus
+    '''
+    sequence = []
+    #so viele buchstaben müssen übereinstimmen
+    for c in range(len(name)):
+        sequence.append(name[c])
+    return sequence
+
+
+
+def proof_avb(data, p,  res_set ):
+    '''
+
+    :param data: Datenbank in der die devices hinterlegt werden
+    :param p: % die übereinstimmen sollen
+    :param  res_set: Die ausgelesenen sequenzen welche noch transformiert werden um ausgelesen werden zukönnen
+            (Die rückgabe von text_rec =  res_set)
+    :return:
+    '''
+    result = []
+    for i in range(len(res_set)):
+       seq = similarity(res_set[i].lower())
+       for d in data.keys():
+            perc = len(d) * p  # p muss in 0.xx angegeben werden
+            ref_d = similarity(d.lower())
+            counter = 0
+            for c in seq:
+                if c in ref_d:
+                    counter += 1
+                    del ref_d[ref_d.index(c)]
+                #print(ref_d)
+            if (counter/len(d)) > p:
+                print(f"Die Wörter {d} und {seq} passen zu { (counter/len(d))*100}% {counter}übereinstimmungen, {perc} =perc")
+                result.append(seq)
+    #result = [set(result)]
+    return result
+            #else:
+                #print(f"Die Wörter {d} und {seq} passen zu {(counter/len(d))*100}% {counter}übereinstimmungen")
+                #return False
+
+#proof_avb(data, 0.7, seq= similarity("ASR90cisco00Series"))
+#proof_avb(d, 0.7, seq= similarity("name"))
+
+result = proof_avb(data,0.70,res_set)
+
 
