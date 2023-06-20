6c67e0b78cba4474697e556ac1c3ccc8c3a80170
Paulina Heine <105652581+PaulinaHeine@users.noreply.github.com>
2023-06-20 11:16:51 +0200

    Update image_read.py

--- Text rec/image_read.py
+++ Text rec/image_read.py
@@ -1,22 +1,23 @@
-
-from Editing_pictures import canny, get_grayscale
+from Text.Pic_edit import canny, get_grayscale
 import pytesseract
 import cv2
+import matplotlib as plt
 
-pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/5.3.1/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Paulina.Heine\AppData\Local\Tesseract-OCR\tesseract.exe"
+X
+X
+X
+X
+X
+def read_picture(pfad):
+    img = cv2.imread(pfad)
+    img_grey = get_grayscale(img)
+    img_canny = canny(img_grey)
+    images = [img_grey, img_canny]
+    configs = [r'--oem 3 --psm 6', r'--psm 4', r"--psm 11 --oem 3"]
 
+    return images, configs
 
-img = cv2.imread('/Users/paulinaheine/s-l1600.jpg')
-#img = cv2.imread('/Users/paulinaheine/IMG_3120.png')
-#img_canny = canny(img)
-#img = cv2.imread("/Users/paulinaheine/Bildschirm­foto 2023-05-10 um 12.26.59.png")
-
-img_grey = get_grayscale(img)
-img_canny = canny(img_grey)
-# plt.imshow(img)
-
-images = [img_grey,img_canny]
-configs = [r'--oem 3 --psm 6', r'--psm 4', r"--psm 11 --oem 3"]
 
 
 def text_rec(images, configs):
@@ -31,22 +32,22 @@ def text_rec(images, configs):
         for i in images:
             print("...loading...")
             # Alle sinnvollen Einstellungen werden durchprobiert
-            #print("read")
+            # print("read")
             res_list = pytesseract.image_to_string(i, config=con)
             # Alle Einträge werden in einzelne snippets unterteilt
-            #print("split")
+            # print("split")
             res_list_2 = res_list.split("\n")
             # Alle leeren Einträge werden entfernt
-            #print("blanks")
+            # print("blanks")
             while '' in res_list_2:
                 res_list_2.remove('')
-            #print("isalnum")
-            #Sonderzeichen entfernen
+            # print("isalnum")
+            # Sonderzeichen entfernen
             for x in range(len(res_list_2)):
                 res_set.add(''.join(filter(str.isalnum, res_list_2[x])))
 
     # Alle Einträge mit weniger als 3 Einträgen werden entferht
-    #print("charlimit")
+    # print("charlimit")
     res_set = list(filter(lambda el: len(el) > 3, res_set))
     res_set = list(filter(lambda el: len(el) < 30, res_set))
     print("done")
@@ -54,5 +55,5 @@ def text_rec(images, configs):
     print(f"We have {len(res_set)}entries")
     return res_set
 
+#res_set = text_rec(images, configs)
 
-#x = text_rec(images, configs)
